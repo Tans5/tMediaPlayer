f4dedd8b5c0aba8e82c4ee612818cd5c3269bec0
tans.tan <810555251@qq.com>
2022-04-10 15:02:08 +0800

    Remove headers.

--- tmediaplayer/src/main/cpp/tmediaplayer.cpp
+++ tmediaplayer/src/main/cpp/tmediaplayer.cpp
@@ -1,10 +1,14 @@
 #include <jni.h>
 #include <string>
+extern "C" {
+#include <libavformat/avformat.h>
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_tans_tmediaplayer_NativeLib_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    AVFormatContext* context = avformat_alloc_context();
+    std::string hello = context->codec_whitelist;
     return env->NewStringUTF(hello.c_str());
 }
