68d3dbc5b282a71a5b376ac2eb29cb89f5a2dbb5
tanpengcheng <tanpengcheng@inke.cn>
2023-08-08 17:28:49 +0800

    [feat] Adjust ascii vertex.

--- tmediaplayer/src/main/java/com/tans/tmediaplayer/render/filter/AsciiArtImageFilter.kt
+++ tmediaplayer/src/main/java/com/tans/tmediaplayer/render/filter/AsciiArtImageFilter.kt
@@ -75,7 +75,13 @@ class AsciiArtImageFilter : ImageFilter {
 
 
     override fun recycle() {
-        // TODO:
+        val renderData = renderData.get()
+        if (renderData != null) {
+            this.renderData.set(null)
+            GLES30.glDeleteProgram(renderData.lumaProgram)
+            GLES30.glDeleteTextures(1, intArrayOf(renderData.lumaTexture), 0)
+            GLES30.glDeleteBuffers(1, intArrayOf(renderData.lumaVbo), 0)
+        }
     }
 
     private fun ensureRenderData(context: Context): RenderData? {
@@ -85,20 +91,22 @@ class AsciiArtImageFilter : ImageFilter {
         } else {
             val lumaProgram = compileShaderProgram(context, R.raw.t_media_player_ascii_filter_luma_vert, R.raw.t_media_player_ascii_filter_luma_frag)
             if (lumaProgram != null) {
-                val lumaVao = glGenVertexArrays()
-                val lumaVbo = glGenFrameBuffer()
                 val lumaTexture = glGenTextureAndSetDefaultParams()
-                GLES30.glBindVertexArray(lumaVao)
-                GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, lumaVbo)
                 val vertices = floatArrayOf(
                     // 坐标(position 0)   // 纹理坐标
-                    -1.0f, 1.0f,   0.0f, 1.0f,    // 左上角
-                    1.0f, 1.0f,    1.0f, 1.0f,   // 右上角
-                    1.0f, -1.0f,   1.0f, 0.0f,   // 右下角
-                    -1.0f, -1.0f,  0.0f, 0.0f,   // 左下角
+                    -1.0f, 1.0f, 0.0f,   0.0f, 1.0f,    // 左上角
+                    1.0f, 1.0f, 0.0f,    1.0f, 1.0f,   // 右上角
+                    1.0f, -1.0f, 0.0f,   1.0f, 0.0f,   // 右下角
+                    -1.0f, -1.0f, 0.0f,  0.0f, 0.0f,   // 左下角
                 )
-                GLES30.glVertexAttribPointer(0, 4, GLES30.GL_FLOAT, false, 16, 0)
+                val lumaVao = glGenVertexArrays()
+                val lumaVbo = glGenFrameBuffer()
+                GLES30.glBindVertexArray(lumaVao)
+                GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, lumaVbo)
+                GLES30.glVertexAttribPointer(0, 3, GLES30.GL_FLOAT, false, 20, 0)
                 GLES30.glEnableVertexAttribArray(0)
+                GLES30.glVertexAttribPointer(1, 2, GLES30.GL_FLOAT, false, 20, 12)
+                GLES30.glEnableVertexAttribArray(1)
                 GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, vertices.size * 4, vertices.toGlBuffer(), GLES30.GL_STATIC_DRAW)
                 val renderData = RenderData(
                     lumaProgram = lumaProgram,
--- tmediaplayer/src/main/res/raw/t_media_player_ascii_filter_luma_vert.vert
+++ tmediaplayer/src/main/res/raw/t_media_player_ascii_filter_luma_vert.vert
@@ -1,9 +1,9 @@
 #version 300 es
-
-layout (location = 0) in vec4 vert;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoord;
 out vec2 TexCoord;
 
 void main() {
-    gl_Position = vec4(vert.xy, 0.0, 1.0);
-    TexCoord = vert.zw;
+    gl_Position = vec4(aPos, 1.0);
+    TexCoord = aTexCoord;
 }
