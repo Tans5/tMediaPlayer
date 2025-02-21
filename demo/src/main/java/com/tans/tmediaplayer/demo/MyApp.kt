59c4fea05799f389effb14215bb33db99c671d25
anonymous <anonymous@inke.cn>
2025-02-21 11:21:29 +0800

    [feat] Add v1.5.2.

--- demo/src/main/java/com/tans/tmediaplayer/demo/MyApp.kt
+++ demo/src/main/java/com/tans/tmediaplayer/demo/MyApp.kt
@@ -1,11 +1,13 @@
 package com.tans.tmediaplayer.demo
 
 import android.app.Application
+import com.tans.tmediaplayer.tMediaPlayerLog
 import com.tans.tuiutils.systembar.AutoApplySystemBarAnnotation
 
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         AutoApplySystemBarAnnotation.init(this)
+        // tMediaPlayerLog.logLevel = tMediaPlayerLog.LogLevel.Error
     }
 }
